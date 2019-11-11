b632d739cfd3403edc7413b003454e516c2dc359
Junjie Chen <chuckjunjchen@gmail.com>
2019-11-10 23:25:48 -0500

    update work laptop

--- user.fish
+++ user.fish
@@ -2,6 +2,9 @@ alias g="git"
 alias l="ll"
 alias v="vim"
 
+alias wm="~/repos/wolfman/bin/wolfman"
+alias be="bundle exec"
+
 set -g theme_color_scheme gruvbox
 
 source /usr/local/opt/asdf/asdf.fish
