0498c8145fbed7ee075008aa51356cb49ec34c08
Junjie Chen <chuckjunjchen@gmail.com>
2019-11-15 16:03:21 -0500

    I like asdf

--- user.fish
+++ user.fish
@@ -3,9 +3,10 @@ alias be="bundle exec"
 
 alias maketags="ag -l | ctags --links=no -L-"
 
+alias v="f -e vim""
+
 alias setjavahome="source ~/.asdf/plugins/java/set-java-home.fish"
 
 set -g theme_color_scheme gruvbox
 
 source /usr/local/opt/asdf/asdf.fish
-
