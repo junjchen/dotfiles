d01c670cf2645c58f172f1854b44019082037f98
junjchen <chuckjunjchen@gmail.com>
2019-12-30 00:29:19 -0500

    revert user.fish

--- user.fish
+++ user.fish
@@ -1,2 +1,13 @@
+alias wm="~/repos/wolfman/bin/wolfman"
+alias be="bundle exec"
 alias v="f -e vim"
+alias vg="vagrant"
+alias mctags="ag -l | ctags --links=no -L-"
+alias sjhome="source ~/.asdf/plugins/java/set-java-home.fish"
+
+set -xg ECLIPSE_HOME "/Applications/Eclipse\ Java.app"
+alias eclimd="sh $ECLIPSE_HOME/Contents/Eclipse/eclimd"
+
 set -g theme_color_scheme gruvbox
+
+source /usr/local/opt/asdf/asdf.fish
