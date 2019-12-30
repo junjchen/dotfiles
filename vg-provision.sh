8cfd3116315ae181a16f4f5cc6a37a3151e8edb8
junjchen <chuckjunjchen@gmail.com>
2019-12-30 00:24:20 -0500

    update shell and fish

--- vg-provision.sh
+++ vg-provision.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
+HOME="/home/vagrant"
+
+add-apt-repository ppa:aacebedo/fasd
 apt-get update
-apt-get -y install git curl silversearcher-ag fish
+apt-get -y install git curl silversearcher-ag fish fasd
 
-chsh -s /usr/bin/fish
+sudo chsh -s /usr/bin/fish vagrant
 
-mkdir -p /home/vagrant/repos
-git clone https://github.com/junjchen/dotfiles.git /home/vagrant/repos/dotfiles
-ln -s /home/vagrant/repos/dotfiles/.vimrc /home/vagrant/.vimrc
+mkdir -p "$HOME/repos"
+
+if [ ! -d "$HOME/repos/dotfiles" ]; then
+    git clone https://github.com/junjchen/dotfiles.git "$HOME/repos/dotfiles";
+fi
+
+if [ ! -e "$HOME/.vimrc" ]; then
+    ln -s "$HOME/repos/dotfiles/.vimrc" "$HOME/.vimrc";
+fi
+
+if [ ! -e "/tmp/install-omf" ]; then
+    curl -L https://get.oh-my.fish > /tmp/install-omf
+    fish /tmp/install-omf --noninteractive
+fi
+
+fish -c 'omf install fasd bobthefish https://github.com/jhillyerd/plugin-git'
