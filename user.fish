74640f417d19df38c6b54b6c417e956ff2f09f23
Junjie Chen <chuckjunjchen@gmail.com>
2020-02-03 15:51:03 -0500

    update user.fish

--- user.fish
+++ user.fish
@@ -1,15 +1,8 @@
+source /usr/local/opt/asdf/asdf.fish
+
 alias wm="~/repos/wolfman/bin/wolfman"
 alias be="bundle exec"
-alias v="f -e vim"
-alias vg="vagrant"
-alias mctags="ag -l | ctags --links=no -L-"
-alias sjhome="source ~/.asdf/plugins/java/set-java-home.fish"
-
-set -xg ECLIPSE_HOME "/Applications/Eclipse\ Java.app"
-alias eclimd="sh $ECLIPSE_HOME/Contents/Eclipse/eclimd"
-
-set -xg PATH $PATH (yarn global bin)
+alias vv="f -e vim"
 
 set -g theme_color_scheme gruvbox
-
-source /usr/local/opt/asdf/asdf.fish
+set -xg PATH $PATH (yarn global bin)
