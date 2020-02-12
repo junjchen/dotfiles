58b509ae59636939ed8aa6663ab49588579d3605
Junjie Chen <chuckjunjchen@gmail.com>
2020-02-12 14:01:54 -0500

    user.fish to config.fish

--- config.fish
+++ config.fish
@@ -7,6 +7,11 @@ alias be="bundle exec"
 alias vv="f -e vim"
 
 set -g theme_color_scheme gruvbox
+
 if command --search yarn >/dev/null do
     set -xg PATH $PATH (yarn global bin)
+end
+
+if command --search pritunlctl >/dev/null do
+  alias vpn="pritunlctl start"
 end
