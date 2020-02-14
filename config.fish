87009f3d77c58a878c981f4400e756ef1c2781b8
junjchen <chuckjunjchen@gmail.com>
2020-02-13 21:15:47 -0500

    check asdf installed

--- config.fish
+++ config.fish
@@ -1,12 +1,14 @@
 fish_vi_key_bindings
 
-source /usr/local/opt/asdf/asdf.fish
-
 alias wm="~/repos/wolfman/bin/wolfman"
 alias be="bundle exec"
 alias vv="f -e vim"
 
 set -g theme_color_scheme gruvbox
+
+if test -e /usr/local/opt/asdf/asdf.fish
+  source /usr/local/opt/asdf/asdf.fish
+end
 
 if command --search yarn >/dev/null do
     set -xg PATH $PATH (yarn global bin)
