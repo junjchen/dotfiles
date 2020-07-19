bf7942c8984ee01c8ae97b154b24865b6a80088b
Junjie Chen <chuckjunjchen@gmail.com>
2020-07-19 00:11:58 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,20 +1,9 @@
 fish_vi_key_bindings
 
-alias wm="~/repos/wolfman/bin/wolfman"
-alias be="bundle exec"
-alias vv="f -e vim"
-alias vg="vagrant"
-
-set -g theme_color_scheme solarized-dark
-
 if test -e ~/.asdf/asdf.fish
   source ~/.asdf/asdf.fish
 end
 
 if command --search yarn >/dev/null do
   set -xg PATH $PATH (yarn global bin)
-end
-
-if command --search pritunlctl >/dev/null do
-  alias vpn="pritunlctl start"
 end
